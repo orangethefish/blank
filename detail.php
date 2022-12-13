163a73d0136067df3fa97caf329be90614637fb8
Nguyễn Duy Hòa <hoa.nguyen2020@hcmut.edu.vn>
2022-12-13 10:19:46 +0700

    update add comment

--- detail.php
+++ detail.php
@@ -34,6 +34,19 @@ if ($result->num_rows > 0)
    }
 }
 CloseCon($conn);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['comment'])) {
+        $conn = OpenCon();
+        $sql = "INSERT INTO news_comment (post_no, owner, date, detail)
+        VALUES ('.$id.', 'Orange','".$date."','".$_POST['comment']."')";
+
+if ($conn->query($sql) === TRUE) {
+//   echo "New record created successfully";
+} else {
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+}
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -84,8 +97,8 @@ CloseCon($conn);
                         <!-- Post content-->
                         <section class="mb-5">
                             <p class="fs-5 mb-4"><?php echo $detail ?></p>
-                            <p class="fs-5 mb-4">The universe is large and old, and the ingredients for life as we know it are everywhere, so there's no reason to think that Earth would be unique in that regard. Whether of not the life became intelligent is a different question, and we'll see if we find that.</p>
-                            <p class="fs-5 mb-4">If you get asteroids about a kilometer in size, those are large enough and carry enough energy into our system to disrupt transportation, communication, the food chains, and that can be a really bad day on Earth.</p>
+                            <!-- <p class="fs-5 mb-4">The universe is large and old, and the ingredients for life as we know it are everywhere, so there's no reason to think that Earth would be unique in that regard. Whether of not the life became intelligent is a different question, and we'll see if we find that.</p>
+                            <p class="fs-5 mb-4">If you get asteroids about a kilometer in size, those are large enough and carry enough energy into our system to disrupt transportation, communication, the food chains, and that can be a really bad day on Earth.</p> -->
                             <!-- <h2 class="fw-bolder mb-4 mt-5">I have odd cosmic thoughts every day</h2>
                             <p class="fs-5 mb-4">For me, the most fascinating interface is Twitter. I have odd cosmic thoughts every day and I realized I could hold them to myself or share them with people who might be interested.</p>
                             <p class="fs-5 mb-4">Venus has a runaway greenhouse effect. I kind of want to know what happened there because we're twirling knobs here on Earth without knowing the consequences of it. Mars once had running water. It's bone dry today. Something bad happened there as well.</p> -->
@@ -96,40 +109,74 @@ CloseCon($conn);
                         <div class="card bg-light">
                             <div class="card-body">
                                 <!-- Comment form-->
-                                <form class="mb-4"><textarea class="form-control" rows="3" placeholder="Join the discussion and leave a comment!"></textarea></form>
+                                <form class="mb-4" method="POST" action="">
+                                    <textarea class="form-control" rows="3" placeholder="Join the discussion and leave a comment!" name="comment"></textarea>
+                                    <div class="form-actions my-2">
+                                        <button type="submit" class="btn btn-success pull-right" name="submit">
+                                            Submit <i class="fa fa-arrow-circle-right"></i>
+                                        </button>
+                                    </div>
+                                </form>
                                 <!-- Comment with nested comments-->
-                                <div class="d-flex mb-4">
+                                <!-- <div class="d-flex mb-4"> -->
                                     <!-- Parent comment-->
-                                    <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+                                    <!-- <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
                                     <div class="ms-3">
                                         <div class="fw-bold">Commenter Name</div>
-                                        If you're going to lead a space frontier, it has to be government; it'll never be private enterprise. Because the space frontier is dangerous, and it's expensive, and it has unquantified risks.
+                                        If you're going to lead a space frontier, it has to be government; it'll never be private enterprise. Because the space frontier is dangerous, and it's expensive, and it has unquantified risks. -->
                                         <!-- Child comment 1-->
-                                        <div class="d-flex mt-4">
+                                        <!-- <div class="d-flex mt-4">
                                             <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
                                             <div class="ms-3">
                                                 <div class="fw-bold">Commenter Name</div>
                                                 And under those conditions, you cannot establish a capital-market evaluation of that enterprise. You can't get investors.
                                             </div>
-                                        </div>
+                                        </div> -->
                                         <!-- Child comment 2-->
-                                        <div class="d-flex mt-4">
+                                        <!-- <div class="d-flex mt-4">
                                             <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
                                             <div class="ms-3">
                                                 <div class="fw-bold">Commenter Name</div>
                                                 When you put money directly to a problem, it makes a good headline.
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
+                                    </div> -->
+                                <!-- </div> -->
                                 <!-- Single comment-->
-                                <div class="d-flex">
+                                <?php
+                                $conn = OpenCon();
+                                $query ="SELECT * FROM `news_comment` ORDER BY date ASC";
+                                //echo "Connected Successfully";
+                                $result=$conn->query($query);
+                                if ($result->num_rows > 0)
+                                {
+                                   // OUTPUT DATA OF EACH ROW
+                                   while($row = $result->fetch_assoc())
+                                   {
+                                       if($row['post_no']==$id){
+                                            echo '<div class="d-flex my-2">
+                                    <div class="flex-shrink-0"><img class="rounded-circle" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png" width="40px" alt="..." /></div>
+                                    <div class="ms-3 container">
+                                        <div class="fw-bold">'.$row['owner'].'</div>
+                                        '.$row['detail'].'
+                                        </div>
+                                        <button class="btn btn-danger btn-sm">Delete</button>
+                                    </div>';
+                                       }
+                                   }
+                                }
+                                else {
+                                   echo "0 results";
+                                }
+                                CloseCon($conn);
+                                ?>
+                                <!-- <div class="d-flex">
                                     <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
                                     <div class="ms-3">
                                         <div class="fw-bold">Commenter Name</div>
                                         When I look at the universe and all the ways the universe wants to kill us, I find it hard to reconcile that with statements of beneficence.
                                     </div>
-                                </div>
+                                </div> -->
                             </div>
                         </div>
                     </section>
